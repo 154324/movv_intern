7a585aae77868618ec7092fa955a0285a5e8758e
154324 <pkw961028@naver.com>
2022-02-15 14:22:22 +0900

    vue componets

---
+++ vueStart/learn-vue-js-master/getting-started/App.vue
@@ -0,0 +1,23 @@
+<template lang="">
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ count }}</p>
+    <button @click="count++">추가</button>
+  </div>
+</template>
+<script>
+import HomeCompenet from './Home';
+
+export default {
+  components: {
+    HomeCompenet,
+  },
+  data() {
+    return {
+      title: '안녕하세요.',
+      count: 1,
+    };
+  },
+};
+</script>
+<style lang=""></style>
