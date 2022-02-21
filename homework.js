f97c04111a8855d32ff017b76c7da47123dad716
154324 <pkw961028@naver.com>
2022-02-22 01:57:36 +0900

    다차원 배열 일차원 만들기

---
+++ homework.js
@@ -0,0 +1,18 @@
+let arr =[  1, 2, [3, 4, [5, 6, 7], 8, 9 ], [10, 11,  [12, 13, [14, 15], 16], 17, [18, 19, 20]]]
+
+
+
+function fn1(arr) {
+    let res = [];
+    arr.map(item => {
+        if(Array.isArray(item)) {
+            res = res.concat(fn1(item));
+        } else {
+            res.push(item);
+        }
+    });
+    return res;
+}
+
+
+console.log(fn1(arr))
